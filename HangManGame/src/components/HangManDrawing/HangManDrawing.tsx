d2a469c2cb16f38e68985872d421d7b12aea7bab
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-08-27 14:02:34 +0530

    HangManDrawing is completed

--- HangManGame/src/components/HangManDrawing/HangManDrawing.tsx
+++ HangManGame/src/components/HangManDrawing/HangManDrawing.tsx
@@ -1,5 +1,27 @@
+import styles from "./HangManDrawing.module.css";
+
+const manHead = <div className={styles.manHead}></div>;
+const manBody = <div className={styles.manBody}></div>;
+const manLeftHand = <div className={styles.manLeftHand}></div>;
+const manRightHand = <div className={styles.manRightHand}></div>;
+const manLeftLeg = <div className={styles.manLeftLeg}></div>;
+const manRightLeg = <div className={styles.manRightLeg}></div>;
+
 const HangManDrawing = () => {
-  return <div>HangManDrawing</div>;
+  return (
+    <div className={styles.stand}>
+      {manHead}
+      {manBody}
+      {manLeftHand}
+      {manRightHand}
+      {manLeftLeg}
+      {manRightLeg}
+      <div className={styles.standHead}></div>
+      <div className={styles.standTop}></div>
+      <div className={styles.standMiddle}></div>
+      <div className={styles.standBottom}></div>
+    </div>
+  );
 };
 
 export default HangManDrawing;
