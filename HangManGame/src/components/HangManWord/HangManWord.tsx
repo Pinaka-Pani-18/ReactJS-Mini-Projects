3dfd3e03309b5ac8c6f25aa23701aacd02d63505
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-08-27 14:22:15 +0530

    HangManWord is completed

--- HangManGame/src/components/HangManWord/HangManWord.tsx
+++ HangManGame/src/components/HangManWord/HangManWord.tsx
@@ -1,5 +1,27 @@
+import styles from "./HangManWord.module.css";
+
 const HangManWord = () => {
-  return <div>HangManWord</div>;
+  const word = "apple";
+  const lettersAvailable = ["a", "l"];
+  return (
+    <div className={styles.hangManWordContainer}>
+      {word.split("").map((letter, index) => {
+        return (
+          <span className={styles.letter} key={index}>
+            <span
+              style={{
+                visibility: lettersAvailable.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HangManWord;
