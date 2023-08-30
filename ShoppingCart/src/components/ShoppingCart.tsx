6388e2850acfefac61f5430525bd15f5b9883449
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-08-30 23:33:23 +0530

    Total amount is added

--- ShoppingCart/src/components/ShoppingCart.tsx
+++ ShoppingCart/src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { UseShoppingCart } from "../context/ShoppingContext";
 import CartItem from "./CartItem";
+import { formatCurrency } from "../utilities/formatCurrency";
+import stoteItems from "../data/items.json";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -18,6 +20,15 @@ const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
           {cartItems.map((item) => {
             return <CartItem key={item.id} {...item} />;
           })}
+          <div className="ms-auto fw-bold fs-5 align-self-end">
+            Total:{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = stoteItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
