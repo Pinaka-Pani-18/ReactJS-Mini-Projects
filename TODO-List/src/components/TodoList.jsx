849c7beeb4309ba7bfd2c8ef0c3593759c5f95ae
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-09-05 21:53:37 +0530

    added extra logic to show the tasks

--- TODO-List/src/components/TodoList.jsx
+++ TODO-List/src/components/TodoList.jsx
@@ -63,30 +63,36 @@ const TodoList = () => {
           </svg>
         </button>
       </div>
-      <div className="flex w-[40rem] justify-between gap-7 mt-7 mb-2">
-        <h3 className=" font-bold rounded-full text-black bg-white py-3 px-7">
-          TASKS :
-        </h3>
-        <button
-          className=" rounded-full text-black bg-white px-4 hover:text-red-500"
-          onClick={() => dispatch({ type: actions.DELETEALL })}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-            />
-          </svg>
-        </button>
-      </div>
+      {state.Tasks.length > 0 ? (
+        <div className="flex w-[40rem] justify-between gap-7 mt-7 mb-2">
+          <h3 className=" font-bold rounded-full text-black bg-white py-3 px-7">
+            TASKS :
+          </h3>
+          {state.Tasks.length > 1 && (
+            <button
+              className=" rounded-full text-black bg-white px-4 hover:text-red-500"
+              onClick={() => dispatch({ type: actions.DELETEALL })}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+      ) : (
+        <h3 className="mt-5 ">No Tasks has been added</h3>
+      )}
       <ul className="w-[40rem] h-auto overflow-y-auto flex flex-col overflow-hidden bg-black rounded-2xl shadow-xl">
         {state.Tasks.map((item, index) => {
           return (
